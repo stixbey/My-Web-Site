5d668feecabf3487e49cd47dbf98ad759abacafd
stixbey <96886240+stixbey@users.noreply.github.com>
2022-08-21 07:39:54 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -71,9 +71,9 @@ function MyApp({ Component, pageProps }) {
         <div className="max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
           <div className="md:flex w-full items-center justify-between">
             <div>
-              <p>❤️ Stix</p></div>
+              <p>❤️ Gbot</p></div>
             <div className="mt-2 md:mt-0 flex items-center">
-              <a href="https://discord.gg/58e5H4try3" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
+              <a href="https://discord.gg/GRkYArtwrk" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
                 <i className="fab fa-discord mr-2" />Join My Discord
               </a>
             </div>
