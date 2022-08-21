4a3d3249c8b79ddcbd8013ddde76d6cd2c6b53f7
stixbey <96886240+stixbey@users.noreply.github.com>
2022-08-21 07:56:14 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
     return <>
         <div className="w-full border-b-2 border-neutral-800/20 pb-2">
             <div className="flex flex-col md:flex-row w-full items-center md:justify-between">
-                <p className="font-semibold font-Poppins text-xl">UB</p>
+                <p className="font-semibold font-Poppins text-xl">Welcome Stix Web Site!</p>
                 <div className="flex items-center space-x-2">
                     {items.map(item => (
                         <a key={item.link} href={item.link} target="_blank" rel="noreferrer" className="flex items-center justify-center hover:bg-neutral-700/20 rounded-xl transition-all duration-150 p-2 px-3">
