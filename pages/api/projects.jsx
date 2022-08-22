b64781283b399ada7d32a90069dae605d6cf6166
stixbey <96886240+stixbey@users.noreply.github.com>
2022-08-22 03:20:07 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -3,15 +3,15 @@ export default async (req, res) => {
     {
       id: 1,
       image: "https://cdn.discordapp.com/attachments/974404943383511080/1011066691238768721/PentagonePartner.png",
-      name: "G Shop and Bot",
+      name: "Gbot and Shop",
       description:
         "hi i am stix, i have been working on a project for a long time and i decided to bring this project to life. The Gbot system project is now waiting for everyone with you!",
       link: "https://discord.gg/GRkYArtwrk",
     },
     {
       id: 2,
-      image: "https://cdn.discordapp.com/attachments/974404943383511080/1010767072520839248/YouTube_full-color_icon_2017.png",
-      name: "Youtube",
+      image: "https://cdn.discordapp.com/attachments/974404943383511080/1011067026367860736/PentagoneGame.png",
+      name: "Youtube Channel",
       description:
         "hi I am a professional discord and Fivem and middleware developer. I have been professionally involved in community management and development for 2 years and have high-level knowledge and experience in areas such as community management and social media management. I share videos about Fivem on my youtube channel.",
       link: "https://www.youtube.com/channel/UCeRG7MNmVQPcW3XSKpDoaTg/featured",
